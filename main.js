ef458a94fad08fa3d5cf363231033377ebdad7d6
Timo Rautiainen <timo.rautiainen94@gmail.com>
2022-10-08 01:05:01 +0300

    do playround function

--- main.js
+++ main.js
@@ -10,3 +10,23 @@ function getComputerChoice() {
 }
 
 console.log(getComputerChoice());
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase;
+    if (playerSelection === computerSelection) {
+        return "It's a draw, nobody wins";
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "You Win! Rock beats Scissors";
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        return "You Lose! Paper beats Rock";
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "You Win! Paper beats Rock";
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        return "You Lose! Scissors beats Paper"; 
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return "You Lose! Rock beats Scissors";
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "You Win! Scissors beats Paper"
+    }
+    
+}
