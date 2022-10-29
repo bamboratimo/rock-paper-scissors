183ff6117b196f747d1c31d3a21a0034e33f8fc4
Timo Rautiainen <timo.rautiainen94@gmail.com>
2022-10-29 18:15:27 +0300

    fix choice imgs, add a header

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ const compPoints = document.querySelector(".compPoints");
 const roundCount = document.querySelector(".roundCount");
 const myChoice = document.querySelector(".myChoice");
 const compChoice = document.querySelector(".compChoice");
+const header = document.querySelector(".header");
 
 let rounds = 0;
 let myScore = 0;
@@ -22,12 +23,15 @@ scissors.addEventListener("click", game);
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
     if (choice === 0) {
+        compChoice.style.visibility = "visible";
         compChoice.src = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/904a8656-d025-458b-ab94-7335122cc9b9/dasxgjv-aadf4828-c1a7-4d36-ac3c-6ec4e402cf53.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzkwNGE4NjU2LWQwMjUtNDU4Yi1hYjk0LTczMzUxMjJjYzliOVwvZGFzeGdqdi1hYWRmNDgyOC1jMWE3LTRkMzYtYWMzYy02ZWM0ZTQwMmNmNTMucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.7qptcRp5A3OEmsIPvPibamZxFihxAWj38OQNnPrpgdg"
         return "rock"; 
     } else if (choice === 1) {
+        compChoice.style.visibility = "visible";
         compChoice.src = "https://cdn.pixabay.com/photo/2017/02/01/12/23/hygiene-2030052_1280.png";
         return "paper";
     } else if (choice === 2) {
+        compChoice.style.visibility = "visible";
         compChoice.src = "https://cdn.pixabay.com/photo/2012/04/01/17/24/scissors-23634_1280.png";
         return "scissors"
     }
@@ -63,17 +67,17 @@ function playRound(playerSelection, computerSelection) {
 
 
 function game(e) {
-        playRound(e.target.className, getComputerChoice());
-        myChoice.src = e.target.src;
-        myPoints.textContent = "My Score: " + myScore;
-        compPoints.textContent = "Computer Score: " + compScore;
-        rounds++;
-        roundCount.textContent = "Rounds: " + rounds;
-        
-        console.log("My score: " + myScore + "\n" + "Computer score: " + compScore);
-        if (myScore === 3) {
-            console.log("You won the game!!!");
-        } else if (compScore === 3) {
-            console.log("Computer won the game :(");
-        }
+    header.style.display = "none";
+    playRound(e.target.className, getComputerChoice());
+    myChoice.style.visibility = "visible";
+    myChoice.src = e.target.src;
+    myPoints.textContent = "My Score: " + myScore;
+    compPoints.textContent = "Computer Score: " + compScore;
+    rounds++;
+    roundCount.textContent = "Rounds: " + rounds;
+    if (myScore === 3) {
+        console.log("You won the game!!!");
+    } else if (compScore === 3) {
+        console.log("Computer won the game :(");
     }
+}
