4cc7390856cc2bc72e8bc076427b43b7ded6a7d3
Timo Rautiainen <timo.rautiainen94@gmail.com>
2022-10-29 16:25:36 +0300

    alter game function and add variables

--- main.js
+++ main.js
@@ -1,10 +1,27 @@
+const rock = document.querySelector(".rock");
+const paper = document.querySelector(".paper");
+const scissors = document.querySelector(".scissors");
+const roundResult = document.querySelector(".roundResult");
+const myPoints = document.querySelector(".myPoints");
+const compPoints = document.querySelector(".compPoints");
+
+let myScore = 0;
+let compScore = 0;
+
+rock.addEventListener("click", game);
+
+paper.addEventListener("click", game);
+
+scissors.addEventListener("click", game);
+
+
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
     if (choice === 0) {
         return "rock"; 
     } else if (choice === 1) {
         return "paper";
-    } else {
+    } else if (choice === 2) {
         return "scissors"
     }
 }
@@ -12,50 +29,39 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        return "It's a draw, nobody gets a score";
+        roundResult.textContent = "It's a draw, nobody gets a score";
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
-        return "You Win! Rock beats Scissors";
+        myScore++;
+        roundResult.textContent = "You Win! " + playerSelection + " beats " + computerSelection;
     } else if (playerSelection === "rock" && computerSelection === "paper") {
-        return "You Lose! Paper beats Rock";
+        compScore++;
+        roundResult.textContent = "You Lose! " + computerSelection + " beats " + playerSelection;
     } else if (playerSelection === "paper" && computerSelection === "rock") {
-        return "You Win! Paper beats Rock";
+        myScore++;
+        roundResult.textContent = "You Win! " + playerSelection + " beats " + computerSelection;
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
-        return "You Lose! Scissors beats Paper"; 
+        compScore++;
+        roundResult.textContent = "You Lose! " + computerSelection + " beats " + playerSelection; 
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
-        return "You Lose! Rock beats Scissors";
+        compScore++;
+        roundResult.textContent = "You Lose! " + computerSelection + " beats " + playerSelection;
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        return "You Win! Scissors beats Paper"
+        myScore++;
+        roundResult.textContent = "You Win! " + playerSelection + " beats " + computerSelection;
     }
 }
 
 
-/*
-function game() {
-    let myScore = 0;
-    let compScore = 0;
-    for (let i = 0; myScore <= 2 && compScore <= 2; i++) {
-        let playerSelection = prompt("Rock, Paper or Scissors?");
-        if (playerSelection === null) {
-            return;
-        }
-        playerSelection = playerSelection.toLowerCase();
-        if (playerSelection != "rock" && playerSelection != "paper" && playerSelection != "scissors") {
-            continue;
-        }
-        const computerSelection = getComputerChoice();
-        let result = playRound(playerSelection, computerSelection);
-        console.log(result);
-        if (result.includes("Win")) {
-            myScore++;
-        } else if (result.includes("Lose")) {
-            compScore++;
-        }
+
+function game(e) {
+        playRound(e.target.className, getComputerChoice());
+        myPoints.textContent = "My Score: " + myScore;
+        compPoints.textContent = "Computer Score " + compScore;
+        
         console.log("My score: " + myScore + "\n" + "Computer score: " + compScore);
-        }
         if (myScore === 3) {
             console.log("You won the game!!!");
         } else if (compScore === 3) {
             console.log("Computer won the game :(");
         }
-}
-*/
+    }
